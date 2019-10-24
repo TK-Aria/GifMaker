2084675ddcc2cfdb0deadcc7f1e0c0b627fb0bd6
tkaria <tkaria.info@gmail.com>
2019-10-24 17:24:39 +0900

    [imp] FileStreamからMemoryStreamへ変更

--- Moments Recorder/Scripts/Gif/GifEncoder.cs
+++ Moments Recorder/Scripts/Gif/GifEncoder.cs
@@ -19,7 +19,9 @@ namespace Moments.Encoder
 		protected int m_Repeat = -1;                  // -1: no repeat, 0: infinite, >0: repeat count
 		protected int m_FrameDelay = 0;               // Frame delay (milliseconds)
 		protected bool m_HasStarted = false;          // Ready to output frames
+		protected MemoryStream m_MemoryStream;
 		protected FileStream m_FileStream;
+		protected System.Action<byte[]> onFinishHandler = null;
 
 		protected GifFrame m_CurrentFrame;
 		protected byte[] m_Pixels;                    // BGR byte array from frame
@@ -122,13 +124,12 @@ namespace Moments.Encoder
 		/// Initiates GIF file creation on the given stream. The stream is not closed automatically.
 		/// </summary>
 		/// <param name="os">OutputStream on which GIF images are written</param>
-		public void Start(FileStream os)
+		public void Start(System.Action<byte[]> onFinish )
 		{
-			if (os == null)
-				throw new ArgumentNullException("Stream is null.");
 
 			m_ShouldCloseStream = false;
-			m_FileStream = os;
+			m_MemoryStream = new MemoryStream();
+			onFinishHandler = onFinish;
 
 			try
 			{
@@ -150,8 +151,8 @@ namespace Moments.Encoder
 		{
 			try
 			{
-				m_FileStream = new FileStream(file, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
-				Start(m_FileStream);
+				m_FileStream = new FileStream(file, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+				Start((bytes) =>  { m_FileStream.Write(bytes, 0, bytes.Length); });
 				m_ShouldCloseStream = true;
 			}
 			catch (IOException e)
@@ -173,11 +174,17 @@ namespace Moments.Encoder
 
 			try
 			{
-				m_FileStream.WriteByte(0x3b); // Gif trailer
-				m_FileStream.Flush();
+				m_MemoryStream.WriteByte(0x3b); // Gif trailer
+
+				var bytes = new byte[(int)m_MemoryStream.Length];
+				m_MemoryStream.Position = 0;
+				m_MemoryStream.Read(bytes, 0, (int)m_MemoryStream.Length);
+				m_MemoryStream.Flush();
+
+				onFinishHandler(bytes);
 
 				if (m_ShouldCloseStream)
-					m_FileStream.Close();
+					m_MemoryStream.Close();
 			}
 			catch (IOException e)
 			{
@@ -185,7 +192,7 @@ namespace Moments.Encoder
 			}
 
 			// Reset for subsequent use
-			m_FileStream = null;
+			m_MemoryStream = null;
 			m_CurrentFrame = null;
 			m_Pixels = null;
 			m_IndexedPixels = null;
@@ -248,25 +255,25 @@ namespace Moments.Encoder
 		// Writes Graphic Control Extension.
 		protected void WriteGraphicCtrlExt()
 		{
-			m_FileStream.WriteByte(0x21); // Extension introducer
-			m_FileStream.WriteByte(0xf9); // GCE label
-			m_FileStream.WriteByte(4);    // Data block size
+			m_MemoryStream.WriteByte(0x21); // Extension introducer
+			m_MemoryStream.WriteByte(0xf9); // GCE label
+			m_MemoryStream.WriteByte(4);    // Data block size
 
 			// Packed fields
-			m_FileStream.WriteByte(Convert.ToByte(0 |     // 1:3 reserved
+			m_MemoryStream.WriteByte(Convert.ToByte(0 |     // 1:3 reserved
 												  0 |     // 4:6 disposal
 												  0 |     // 7   user input - 0 = none
 												  0));    // 8   transparency flag
 
 			WriteShort(m_FrameDelay); // Delay x 1/100 sec
-			m_FileStream.WriteByte(Convert.ToByte(0)); // Transparent color index
-			m_FileStream.WriteByte(0); // Block terminator
+			m_MemoryStream.WriteByte(Convert.ToByte(0)); // Transparent color index
+			m_MemoryStream.WriteByte(0); // Block terminator
 		}
 
 		// Writes Image Descriptor.
 		protected void WriteImageDesc()
 		{
-			m_FileStream.WriteByte(0x2c); // Image separator
+			m_MemoryStream.WriteByte(0x2c); // Image separator
 			WriteShort(0);                // Image position x,y = 0,0
 			WriteShort(0);
 			WriteShort(m_Width);          // image size
@@ -275,12 +282,12 @@ namespace Moments.Encoder
 			// Packed fields
 			if (m_IsFirstFrame)
 			{
-				m_FileStream.WriteByte(0); // No LCT  - GCT is used for first (or only) frame
+				m_MemoryStream.WriteByte(0); // No LCT  - GCT is used for first (or only) frame
 			}
 			else
 			{
 				// Specify normal LCT
-				m_FileStream.WriteByte(Convert.ToByte(0x80 |           // 1 local color table  1=yes
+				m_MemoryStream.WriteByte(Convert.ToByte(0x80 |           // 1 local color table  1=yes
 													  0 |              // 2 interlace - 0=no
 													  0 |              // 3 sorted - 0=no
 													  0 |              // 4-5 reserved
@@ -296,50 +303,50 @@ namespace Moments.Encoder
 			WriteShort(m_Height);
 
 			// Packed fields
-			m_FileStream.WriteByte(Convert.ToByte(0x80 |           // 1   : global color table flag = 1 (gct used)
+			m_MemoryStream.WriteByte(Convert.ToByte(0x80 |           // 1   : global color table flag = 1 (gct used)
 												  0x70 |           // 2-4 : color resolution = 7
 												  0x00 |           // 5   : gct sort flag = 0
 												  m_PaletteSize)); // 6-8 : gct size
 
-			m_FileStream.WriteByte(0); // Background color index
-			m_FileStream.WriteByte(0); // Pixel aspect ratio - assume 1:1
+			m_MemoryStream.WriteByte(0); // Background color index
+			m_MemoryStream.WriteByte(0); // Pixel aspect ratio - assume 1:1
 		}
 
 		// Writes Netscape application extension to define repeat count.
 		protected void WriteNetscapeExt()
 		{
-			m_FileStream.WriteByte(0x21);    // Extension introducer
-			m_FileStream.WriteByte(0xff);    // App extension label
-			m_FileStream.WriteByte(11);      // Block size
+			m_MemoryStream.WriteByte(0x21);    // Extension introducer
+			m_MemoryStream.WriteByte(0xff);    // App extension label
+			m_MemoryStream.WriteByte(11);      // Block size
 			WriteString("NETSCAPE" + "2.0"); // App id + auth code
-			m_FileStream.WriteByte(3);       // Sub-block size
-			m_FileStream.WriteByte(1);       // Loop sub-block id
+			m_MemoryStream.WriteByte(3);       // Sub-block size
+			m_MemoryStream.WriteByte(1);       // Loop sub-block id
 			WriteShort(m_Repeat);            // Loop count (extra iterations, 0=repeat forever)
-			m_FileStream.WriteByte(0);       // Block terminator
+			m_MemoryStream.WriteByte(0);       // Block terminator
 		}
 
 		// Write color table.
 		protected void WritePalette()
 		{
-			m_FileStream.Write(m_ColorTab, 0, m_ColorTab.Length);
+			m_MemoryStream.Write(m_ColorTab, 0, m_ColorTab.Length);
 			int n = (3 * 256) - m_ColorTab.Length;
 
 			for (int i = 0; i < n; i++)
-				m_FileStream.WriteByte(0);
+				m_MemoryStream.WriteByte(0);
 		}
 
 		// Encodes and writes pixel data.
 		protected void WritePixels()
 		{
 			LzwEncoder encoder = new LzwEncoder(m_Width, m_Height, m_IndexedPixels, m_ColorDepth);
-			encoder.Encode(m_FileStream);
+			encoder.Encode(m_MemoryStream);
 		}
 
 		// Write 16-bit value to output stream, LSB first.
 		protected void WriteShort(int value)
 		{
-			m_FileStream.WriteByte(Convert.ToByte(value & 0xff));
-			m_FileStream.WriteByte(Convert.ToByte((value >> 8) & 0xff));
+			m_MemoryStream.WriteByte(Convert.ToByte(value & 0xff));
+			m_MemoryStream.WriteByte(Convert.ToByte((value >> 8) & 0xff));
 		}
 
 		// Writes string to output stream.
@@ -348,7 +355,7 @@ namespace Moments.Encoder
 			char[] chars = s.ToCharArray();
 
 			for (int i = 0; i < chars.Length; i++)
-				m_FileStream.WriteByte((byte)chars[i]);
+				m_MemoryStream.WriteByte((byte)chars[i]);
 		}
 	}
 }
